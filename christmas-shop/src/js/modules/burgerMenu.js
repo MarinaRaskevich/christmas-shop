e46e8337d8afdc69b8bee4b492748a9ab4ea3577
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-11-28 19:20:55 +0100

    feat: update menu panel behavior when clicking on non-interactive links

--- christmas-shop/src/js/modules/burgerMenu.js
+++ christmas-shop/src/js/modules/burgerMenu.js
@@ -1,7 +1,7 @@
 export const handleBurgerMenu = () => {
   const hamburger = document.querySelector(".hamburger");
   const navigation = document.querySelector(".header__navigation");
-  const links = document.querySelectorAll(".navigation__link");
+  const navigationLinks = document.querySelector(".navigation");
 
   hamburger.addEventListener("click", () => {
     navigation.classList.toggle("active");
@@ -9,13 +9,16 @@ export const handleBurgerMenu = () => {
     document.body.classList.toggle("scroll-off");
   });
 
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
+  const hideNavigationPanel = (e) => {
+    const link = e.target;
+    if (link.closest(".navigation__link") || link.closest(".navigation")) {
       navigation.classList.remove("active");
       hamburger.classList.remove("active");
       document.body.classList.remove("scroll-off");
-    });
-  });
+    }
+  };
+
+  navigationLinks.addEventListener("click", hideNavigationPanel);
 
   window.addEventListener("resize", () => {
     if (window.innerWidth > 768) {
